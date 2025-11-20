dcd1ad06055930cbd884dc11bc8b5d1f17e70c5c
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-11-19 23:42:19 -0400

    Refactor envs.ts

--- tests/modules/shared/infrastructure/envs.ts
+++ tests/modules/shared/infrastructure/envs.ts
@@ -1,8 +1,5 @@
 import { config } from 'dotenv';
-import path from 'path';
 
-// Cargar .env.test antes de que se ejecuten los tests de Cucumber
 config({
-  path: path.resolve(process.cwd(), '.env.test'),
-  override: true
+  path: '.env.test'
 });
